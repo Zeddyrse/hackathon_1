3b5b920ebef48e391e2760f7a8979c8709f47700
jeremy <jeremy.guimard.pro@gmail.com>
2022-12-01 19:58:16 +0100

    create migrations

---
+++ migrations/Version20221201185410.php
@@ -0,0 +1,49 @@
+<?php
+
+declare(strict_types=1);
+
+namespace DoctrineMigrations;
+
+use Doctrine\DBAL\Schema\Schema;
+use Doctrine\Migrations\AbstractMigration;
+
+/**
+ * Auto-generated Migration: Please modify to your needs!
+ */
+final class Version20221201185410 extends AbstractMigration
+{
+    public function getDescription(): string
+    {
+        return '';
+    }
+
+    public function up(Schema $schema): void
+    {
+        // this up() migration is auto-generated, please modify it to your needs
+        $this->addSql('CREATE TABLE location (id INT AUTO_INCREMENT NOT NULL, travel_id INT NOT NULL, country_name VARCHAR(255) NOT NULL, city_name VARCHAR(255) NOT NULL, INDEX IDX_5E9E89CBECAB15B3 (travel_id), PRIMARY KEY(id)) DEFAULT CHARACTER SET utf8mb4 COLLATE `utf8mb4_unicode_ci` ENGINE = InnoDB');
+        $this->addSql('CREATE TABLE theme (id INT AUTO_INCREMENT NOT NULL, name VARCHAR(255) NOT NULL, PRIMARY KEY(id)) DEFAULT CHARACTER SET utf8mb4 COLLATE `utf8mb4_unicode_ci` ENGINE = InnoDB');
+        $this->addSql('CREATE TABLE theme_travel (theme_id INT NOT NULL, travel_id INT NOT NULL, INDEX IDX_FD335FB259027487 (theme_id), INDEX IDX_FD335FB2ECAB15B3 (travel_id), PRIMARY KEY(theme_id, travel_id)) DEFAULT CHARACTER SET utf8mb4 COLLATE `utf8mb4_unicode_ci` ENGINE = InnoDB');
+        $this->addSql('CREATE TABLE travel (id INT AUTO_INCREMENT NOT NULL, date_start DATETIME NOT NULL, date_end DATETIME NOT NULL, PRIMARY KEY(id)) DEFAULT CHARACTER SET utf8mb4 COLLATE `utf8mb4_unicode_ci` ENGINE = InnoDB');
+        $this->addSql('CREATE TABLE `user` (id INT AUTO_INCREMENT NOT NULL, travel_id INT NOT NULL, firstname VARCHAR(255) NOT NULL, lastname VARCHAR(255) NOT NULL, age INT NOT NULL, sex VARCHAR(255) NOT NULL, email VARCHAR(255) NOT NULL, INDEX IDX_8D93D649ECAB15B3 (travel_id), PRIMARY KEY(id)) DEFAULT CHARACTER SET utf8mb4 COLLATE `utf8mb4_unicode_ci` ENGINE = InnoDB');
+        $this->addSql('CREATE TABLE messenger_messages (id BIGINT AUTO_INCREMENT NOT NULL, body LONGTEXT NOT NULL, headers LONGTEXT NOT NULL, queue_name VARCHAR(190) NOT NULL, created_at DATETIME NOT NULL, available_at DATETIME NOT NULL, delivered_at DATETIME DEFAULT NULL, INDEX IDX_75EA56E0FB7336F0 (queue_name), INDEX IDX_75EA56E0E3BD61CE (available_at), INDEX IDX_75EA56E016BA31DB (delivered_at), PRIMARY KEY(id)) DEFAULT CHARACTER SET utf8mb4 COLLATE `utf8mb4_unicode_ci` ENGINE = InnoDB');
+        $this->addSql('ALTER TABLE location ADD CONSTRAINT FK_5E9E89CBECAB15B3 FOREIGN KEY (travel_id) REFERENCES travel (id)');
+        $this->addSql('ALTER TABLE theme_travel ADD CONSTRAINT FK_FD335FB259027487 FOREIGN KEY (theme_id) REFERENCES theme (id) ON DELETE CASCADE');
+        $this->addSql('ALTER TABLE theme_travel ADD CONSTRAINT FK_FD335FB2ECAB15B3 FOREIGN KEY (travel_id) REFERENCES travel (id) ON DELETE CASCADE');
+        $this->addSql('ALTER TABLE `user` ADD CONSTRAINT FK_8D93D649ECAB15B3 FOREIGN KEY (travel_id) REFERENCES travel (id)');
+    }
+
+    public function down(Schema $schema): void
+    {
+        // this down() migration is auto-generated, please modify it to your needs
+        $this->addSql('ALTER TABLE location DROP FOREIGN KEY FK_5E9E89CBECAB15B3');
+        $this->addSql('ALTER TABLE theme_travel DROP FOREIGN KEY FK_FD335FB259027487');
+        $this->addSql('ALTER TABLE theme_travel DROP FOREIGN KEY FK_FD335FB2ECAB15B3');
+        $this->addSql('ALTER TABLE `user` DROP FOREIGN KEY FK_8D93D649ECAB15B3');
+        $this->addSql('DROP TABLE location');
+        $this->addSql('DROP TABLE theme');
+        $this->addSql('DROP TABLE theme_travel');
+        $this->addSql('DROP TABLE travel');
+        $this->addSql('DROP TABLE `user`');
+        $this->addSql('DROP TABLE messenger_messages');
+    }
+}
