0331a2e55537c3ac6585c412dc31f826f9b07248
jeremy <jeremy.guimard.pro@gmail.com>
2022-12-02 00:10:36 +0100

    Create controller, forms and views

---
+++ src/Form/TravelType.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace App\Form;
+
+use App\Entity\Travel;
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+
+class TravelType extends AbstractType
+{
+    public function buildForm(FormBuilderInterface $builder, array $options): void
+    {
+        $builder
+            ->add('date_start')
+            ->add('date_end')
+            ->add('themes')
+        ;
+    }
+
+    public function configureOptions(OptionsResolver $resolver): void
+    {
+        $resolver->setDefaults([
+            'data_class' => Travel::class,
+        ]);
+    }
+}
